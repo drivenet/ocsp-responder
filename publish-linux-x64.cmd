0840ecd8b9abb877a08f76364ce475a1f990d650
Aristarkh Zagorodnikov <xm@drive.net>
2020-07-08 23:32:24 +0300

    Cleanup publish script

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -4,6 +4,6 @@ mkdir packages\linux-x64\ocsp-responder
 dotnet publish OcspResponder --output packages\linux-x64\ocsp-responder -c Release -r linux-x64 --self-contained false
 move packages\linux-x64\ocsp-responder\Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.dll "%TEMP%"
 move packages\linux-x64\ocsp-responder\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
-del packages\linux-x64\ocsp-responder\web.config packages\linux-x64\ocsp-responder\*.deps.json packages\linux-x64\ocsp-responder\*settings.json packages\linux-x64\ocsp-responder\Microsoft.*.dll
+del packages\linux-x64\ocsp-responder\web.config packages\linux-x64\ocsp-responder\*.deps.json packages\linux-x64\ocsp-responder\Microsoft.*.dll
 move "%TEMP%\Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.dll" packages\linux-x64\ocsp-responder
 move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\ocsp-responder
