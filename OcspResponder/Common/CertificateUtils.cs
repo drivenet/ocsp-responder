6eb2621196b1bb4a2d30d3c98c42d1cd21bdaabe
Aristarkh Zagorodnikov <xm@drive.net>
2020-07-10 17:37:49 +0300

    Add CertificateUtils

---
+++ OcspResponder/Common/CertificateUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace OcspResponder.Common
+{
+    public static class CertificateUtils
+    {
+        public static BigInteger GetSerialNumber(string serial)
+            => BigInteger.Parse(serial, NumberStyles.None, NumberFormatInfo.InvariantInfo);
+
+        public static string ToSerialNumberHexString(BigInteger serial)
+            => Hex.ToHexString(serial.ToByteArray(isUnsigned: true, isBigEndian: true));
+    }
+}
