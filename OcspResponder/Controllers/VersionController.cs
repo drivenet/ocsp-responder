901e99a0c83afa27095a262cf76bb848f250e00c
Aristarkh Zagorodnikov <xm@drive.net>
2020-07-14 16:33:16 +0300

    Add VersionController

---
+++ OcspResponder/Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mime;
+using System.Reflection;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OcspResponder.Controllers
+{
+    public sealed class VersionController : Controller
+    {
+        private static readonly string VersionString = Assembly.GetEntryAssembly()?.GetName().Version?.ToString(3) ?? "?";
+
+        [Route("version")]
+        public ContentResult GetVersion()
+            => Content(VersionString, MediaTypeNames.Text.Plain, Encoding.ASCII);
+    }
+}
