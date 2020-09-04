bbf742c52d6ad2c574a247ddaa87706f6f44f269
Aristarkh Zagorodnikov <xm@drive.net>
2020-09-04 22:38:51 +0300

    Fix publish configuration

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rmdir /s /q packages\linux-x64\ocsp-responder
 mkdir packages\linux-x64\ocsp-responder
-dotnet publish OcspResponder --output packages\linux-x64\ocsp-responder -c Release -r linux-x64 --self-contained false
+dotnet publish OcspResponder --output packages\linux-x64\ocsp-responder -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\ocsp-responder\Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.dll "%TEMP%"
 move packages\linux-x64\ocsp-responder\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 del packages\linux-x64\ocsp-responder\web.config packages\linux-x64\ocsp-responder\*.deps.json packages\linux-x64\ocsp-responder\appsettings.json packages\linux-x64\ocsp-responder\Microsoft.*.dll
