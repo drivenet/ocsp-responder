6885f60add6eaa8784b4db7f51d04af08f613615
Aristarkh Zagorodnikov <xm@drive.net>
2020-11-17 14:41:27 +0300

    Cleanup

--- OcspResponder/Composition/Program.cs
+++ OcspResponder/Composition/Program.cs
@@ -42,7 +42,13 @@ namespace OcspResponder.Composition
 #endif
                 .UseStartup<Startup>();
 
+#if MINIMAL_BUILD
+#pragma warning disable CA1801 // Review unused parameters -- required for other build configuration
+#endif
         private static void ConfigureLogging(HostBuilderContext builderContext, ILoggingBuilder loggingBuilder)
+#if MINIMAL_BUILD
+#pragma warning restore CA1801 // Review unused parameters
+#endif
         {
             loggingBuilder.AddFilter(
                 (category, level) => level >= LogLevel.Warning
