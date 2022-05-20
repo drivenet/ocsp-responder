c6d1a3c2a4259aa93b2321405c72c5faa0feb12a
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-05-20 15:46:21 +0300

    Add minimal build publish

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -3,3 +3,7 @@ rmdir /s /q packages\linux-x64\ocsp-responder
 mkdir packages\linux-x64\ocsp-responder
 dotnet publish OcspResponder --force --output packages\linux-x64\ocsp-responder -c Integration -r linux-x64 --no-self-contained
 del packages\linux-x64\ocsp-responder\*.deps.json
+rmdir /s /q packages\linux-x64\ocsp-responder-minimal
+mkdir packages\linux-x64\ocsp-responder-minimal
+dotnet publish OcspResponder --force --output packages\linux-x64\ocsp-responder-minimal -c Integration -r linux-x64 --no-self-contained -p:MinimalBuild=true
+del packages\linux-x64\ocsp-responder-minimal\*.deps.json
