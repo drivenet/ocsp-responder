3a05c85d4e4b4b23c27959752db0ee56c7e5a4b7
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-06-08 10:12:56 +0300

    Cleanup

--- OcspResponder/Composition/Program.cs
+++ OcspResponder/Composition/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Net;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
--- OcspResponder/Composition/Startup.cs
+++ OcspResponder/Composition/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
